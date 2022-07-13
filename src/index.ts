85faa2e19ca6f77f852e2ebdaa9f4d10a5c68a33
Samuel Jędrzejewski <samuel.jedrzejewski@carrier.com>
2022-07-13 17:41:39 +0200

    Played with arrays and objects

--- src/index.ts
+++ src/index.ts
@@ -1,18 +1,30 @@
 /* eslint-disable prettier/prettier */
 const character = 'SAMI';
-
 console.log(character);
 
 let x: number;
-
 x = 20;
-
 x = 30;
-
 console.log(x);
 
 const circle = (diameter: number) => {
     return diameter*Math.PI;
 }
+console.log(circle(2));
 
-console.log(circle(2))
+const names = ['Sami', 'XD'];
+names.push('');
+console.log(names);
+
+const mixed = ['Kappa', 238, true];
+console.log(mixed);
+
+const Sami = {
+    mixed,
+    age: 21,
+    names,
+    getAge() {
+        return this.age;
+    }
+};
+console.log(Sami);
