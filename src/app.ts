7916700dbf7daccef5d0c4cb735c0993f1a35fd8
Samuel Jędrzejewski <samuel.jedrzejewski@carrier.com>
2022-07-14 10:38:55 +0200

    Set up the environment

---
+++ src/app.ts
@@ -0,0 +1,17 @@
+/* eslint-disable prettier/prettier */
+const anchor = document.querySelector('a');
+console.log(anchor?.href);
+
+const form = document.querySelector('form.new-item-form') as HTMLFormElement;
+console.log(form.method);
+
+const type = document.querySelector('#type') as HTMLInputElement;
+const tofrom = document.querySelector('#tofrom') as HTMLInputElement;
+const details = document.querySelector('#details') as HTMLInputElement;
+const amount = document.querySelector('#amount') as HTMLInputElement;
+console.log(type, tofrom, details, amount);
+
+form.addEventListener('submit', (e: Event) => {
+    e.preventDefault();
+    console.log(type.value, tofrom.value, details.value, amount.valueAsNumber);
+});
