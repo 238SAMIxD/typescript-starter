27547ec845f773b81e316d0c2c96abc641fdc4bb
Samuel Jędrzejewski <samuel.jedrzejewski@carrier.com>
2022-07-14 14:43:36 +0200

    Got rid of debugging and learning code

--- build/app.js
+++ build/app.js
@@ -1,26 +1,11 @@
 /* eslint-disable prettier/prettier */
 import Invoice from './Modules/Invoice.js';
 import Payment from './Modules/Payment.js';
-const me = {
-    name: 'Samuel',
-    age: 21,
-    speak(text) {
-        console.log(text);
-    },
-    spends(amount) {
-        return amount;
-    }
-};
-console.log(me);
-const anchor = document.querySelector('a');
-console.log(anchor === null || anchor === void 0 ? void 0 : anchor.href);
 const form = document.querySelector('form.new-item-form');
-console.log(form.method);
 const type = document.querySelector('#type');
 const tofrom = document.querySelector('#tofrom');
 const details = document.querySelector('#details');
 const amount = document.querySelector('#amount');
-console.log(type, tofrom, details, amount);
 form.addEventListener('submit', (e) => {
     e.preventDefault();
     let doc;
@@ -30,8 +15,5 @@ form.addEventListener('submit', (e) => {
     else {
         doc = new Payment(tofrom.value, details.value, amount.valueAsNumber);
     }
-    console.log(doc.format());
 });
-const inv = new Invoice('Samuel', 'laundry', 20);
-console.error(inv.format());
 //# sourceMappingURL=app.js.map
--- src/app.ts
+++ src/app.ts
@@ -11,29 +11,11 @@ interface Person {
     spends(howMuch: number): number;
 }
 
-const me: Person = {
-    name: 'Samuel',
-    age: 21,
-    speak(text: string) {
-        console.log(text);
-    },
-    spends(amount: number) {
-        return amount;
-    }
-}
-console.log(me);
-
-const anchor = document.querySelector('a');
-console.log(anchor?.href);
-
 const form = document.querySelector('form.new-item-form') as HTMLFormElement;
-console.log(form.method);
-
 const type = document.querySelector('#type') as HTMLInputElement;
 const tofrom = document.querySelector('#tofrom') as HTMLInputElement;
 const details = document.querySelector('#details') as HTMLInputElement;
 const amount = document.querySelector('#amount') as HTMLInputElement;
-console.log(type, tofrom, details, amount);
 
 form.addEventListener('submit', (e: Event) => {
     e.preventDefault();
@@ -44,8 +26,4 @@ form.addEventListener('submit', (e: Event) => {
     } else {
         doc = new Payment(tofrom.value, details.value, amount.valueAsNumber);
     }
-    console.log(doc.format());
 });
-
-const inv = new Invoice('Samuel', 'laundry', 20);
-console.error(inv.format());
